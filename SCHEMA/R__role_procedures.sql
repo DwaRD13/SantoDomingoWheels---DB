2712bca7b9bc8c2abdbf76c69a5ff8f2e972e8e3
DwaRD13 <munozdarwin013@gmail.com>
2025-09-17 16:30:37 -0400

    CRUD PROCEDURES DE ROLE

--- SCHEMA/R__role_procedures.sql
+++ SCHEMA/R__role_procedures.sql
@@ -1,6 +1,8 @@
 DROP PROCEDURE IF EXISTS proc_getAllRolesActive;
 DROP PROCEDURE IF EXISTS proc_getRoleActiveByID;
 DROP PROCEDURE IF EXISTS proc_createRole;
+DROP PROCEDURE IF EXISTS proc_updateRole;
+DROP PROCEDURE IF EXISTS proc_deleteRole;
 
 DELIMITER //
 DELIMITER ;
@@ -9,7 +11,7 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE proc_getAllRolesActive (IN p_state VARCHAR(20))
 BEGIN
-    SELECT R.ID, R.CREATED_AT, R.DESCRIPTION, R.NAME, S.NAME AS STATE
+    SELECT R.ID, R.CREATED_AT, R.DESCRIPTION, R.NAME, S.NAME AS STATE, R.UPDATED_AT
     FROM `ROLE` R
     INNER JOIN `STATE` S ON S.ID = R.STATE_ID
     WHERE S.NAME = p_state;
@@ -21,7 +23,7 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE proc_getRoleActiveByID (IN p_id int)
 BEGIN
-    SELECT R.ID, R.CREATED_AT, R.DESCRIPTION, R.NAME, S.NAME AS STATE
+    SELECT R.ID, R.CREATED_AT, R.DESCRIPTION, R.NAME, S.NAME AS STATE, R.UPDATED_AT
     FROM `ROLE` R
     INNER JOIN `STATE` S ON S.ID = R.STATE_ID
     WHERE R.ID = p_id;
@@ -47,5 +49,42 @@ BEGIN
 END //
 DELIMITER ;
 -----------------------------------------------------------------------------------------------------------------------
+DELIMITER //
+CREATE PROCEDURE proc_updateRole (IN p_id INT, IN p_name VARCHAR(40), IN p_description VARCHAR(40),
+                                                                      IN p_state VARCHAR(40))
+BEGIN
+    DECLARE v_id INT;
+    DECLARE v_updateRole INT;
 
+    SELECT ID INTO v_id
+    FROM `STATE`
+    WHERE NAME = p_state;
+
+    UPDATE `ROLE`
+    SET DESCRIPTION = p_description, NAME = p_name, STATE_ID = v_id, updated_at = CURRENT_TIMESTAMP
+    WHERE ID = p_id;
+
+    CALL proc_getRoleActiveByID(p_id);
+
+END //
+DELIMITER ;
+----------------------------------------------------------------------------------------------------------------------
+DELIMITER //
+CREATE PROCEDURE proc_deleteRole (IN p_id INT)
+BEGIN
+    DECLARE v_id INT;
+
+    SELECT ID INTO v_id
+    FROM `STATE`
+    WHERE NAME = 'DELETE';
+
+    UPDATE `ROLE`
+    SET STATE_ID = v_id, deleted_at = CURRENT_TIMESTAMP
+    WHERE ID = p_id;
+
+    CALL proc_getRoleActiveByID(p_id);
+
+
+END //
+DELIMITER ;
 ----------------------------------------------------------------------------------------------------------------------
