11b99c77769f6de955e24a10cfc164ea7fbc61cf
DwaRD13 <munozdarwin013@gmail.com>
2025-10-04 23:37:58 -0400

    Add inserts

--- SCRIPT/InsertScript_1.sql
+++ SCRIPT/InsertScript_1.sql
@@ -4,10 +4,18 @@
 INSERT INTO STATE (description, name, group_key) VALUES ("EN USO", "Activo", "GENERIC");
 INSERT INTO STATE (description, name, group_key) VALUES ("DETENIDO", "Pausado", "GENERIC");
 INSERT INTO STATE (description, name, group_key) VALUES ("NO USADO", "Borrado", "GENERIC");
-
+INSERT INTO STATE (description, name, group_key) VALUES ("EN USO", "Activo", "GENERIC");
+INSERT INTO STATE (description, name, group_key) VALUES ("Disponible", "AVAILABLE", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("Reservado", "RESERVED", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("En uso", "IN_USE", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("Mantenimiento", "MAINTENANCE", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("Bloqueado", "BLOCKED", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("Detenido por temas sanitarios", "QUARANTINE", "VEHICLE");
+INSERT INTO STATE (description, name, group_key) VALUES ("Retirado",  "RETIRED", "VEHICLE");
 -- ROL 
 INSERT INTO ROLE(DESCRIPTION, NAME, STATE_ID) VALUES ('PUEDE HACER TODO', 'ADMIN', 1);
 INSERT INTO ROLE(DESCRIPTION, NAME, STATE_ID) VALUES ('HACER ALGUNAS COSAS', 'USER', 1);
+INSERT INTO ROLE(DESCRIPTION, NAME, STATE_ID) VALUES ('Hace cositas', 'EMPLOYEE', 1);
 
 -- USER
 INSERT INTO `USER` (USER_NAME, EMAIL, PASSWORD, FIRST_NAME, SECOND_NAME, LAST_NAME, SECOND_SURNAME,
